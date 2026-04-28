81ee8889ee674a79bad0064fdd1210e8b21c19d5
Darja <daria.lipniagowa@gmail.com>
2026-04-28 11:29:59 +0300

    admin moved bottom, and style updated

---
+++ Portfolio/Models/ContactFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your email.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email.")]
+        public string Email { get; set; } = string.Empty;
+
+        public string InquiryType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your message.")]
+        public string Message { get; set; } = string.Empty;
+    }
+}
